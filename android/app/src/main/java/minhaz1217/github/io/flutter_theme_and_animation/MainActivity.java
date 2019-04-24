a579de53729f16d518d95f92632e4e3aa6f8e7af
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-24 23:16:21 +0600

    initial verified commit

---
+++ android/app/src/main/java/minhaz1217/github/io/flutter_theme_and_animation/MainActivity.java
@@ -0,0 +1,13 @@
+package minhaz1217.github.io.flutter_theme_and_animation;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
