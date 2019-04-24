8917e07f8629d2c9f9f7077e906b3dd41c98292e
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-25 03:26:41 +0600

    Dark theme and inherited text color

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,15 @@ class MyApp extends StatelessWidget {
       //debugShowMaterialGrid: true,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        textTheme: new TextTheme(
+          body1: new TextStyle(
+            color: Colors.red,
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Theme and Animation'),
     );
   }
 }
