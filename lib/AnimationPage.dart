be1f4aa4a0b09a310da67056d5a5de43986df869
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-04-26 13:51:16 +0600

    Animated the background

--- lib/AnimationPage.dart
+++ lib/AnimationPage.dart
@@ -20,7 +20,37 @@ class BarLoadingScreen extends StatefulWidget {
   BarLoadingScreenState createState() => new BarLoadingScreenState();
 }
 
-class BarLoadingScreenState extends State<BarLoadingScreen> {
+class BarLoadingScreenState extends State<BarLoadingScreen> with SingleTickerProviderStateMixin{
+
+  AnimationController _controller;
+  Animation<Color> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new AnimationController(
+        duration: const Duration(
+          milliseconds: 3000,
+        ),
+        vsync: this,
+    );
+
+    animation = new ColorTween(
+      begin: const Color.fromRGBO(10,10, 10, 0.5),
+      end: const Color.fromRGBO(0,200, 100, 0.5),
+    ).animate(_controller)
+    ..addListener((){
+      setState(() {});
+    });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -28,6 +58,9 @@ class BarLoadingScreenState extends State<BarLoadingScreen> {
           title: Text("Animation"),
         ),
         body: new Container(
+          decoration: new BoxDecoration(
+            color: animation.value,
+          ),
           child: new Center(
             child: new Row(
               mainAxisAlignment: MainAxisAlignment.center,
